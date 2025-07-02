2a9848ff135c123bfc1b4159cda477bb8fb20b9e
macbookair <amarj234@gmail.com>
2025-07-02 21:34:50 +0530

    test

--- lib/DSA/leetCode/first-missing-positive.dart
+++ lib/DSA/leetCode/first-missing-positive.dart
@@ -29,7 +29,10 @@ void main() {
     int n = nums.length;
 
     for (int i = 0; i < n; i++) {
+      print(nums);
       while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+        print(
+            "${nums[i]} > 0 && ${nums[i]} <= $n && ${nums[nums[i] - 1]} != ${nums[i]}");
         int correctIdx = nums[i] - 1;
         int temp = nums[i];
         nums[i] = nums[correctIdx];
